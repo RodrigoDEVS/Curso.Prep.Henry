431dec32bd93a67f772000bb70e5a0c656a0dd9a
RodrigoDEVS <rodrigo.gallego@outlook.es>
2022-01-12 14:07:07 -0500

    terminado el homework VI

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -4,22 +4,28 @@ function mayuscula(nombre) {
   //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
   //ej: Recibe "mario" ----> Devuelve "Mario"
   //Tu código:
+
+  //se almacena el argumento nombre en una variable
   let name = nombre
+  //se toma el primer caracter de name y se pasa a mayuscula y se almacena en una variable
   let mayuscula = name.charAt(0).toUpperCase();
+  //se extrae la parte restante del String empezando desde el segundo caracter y se almacena en una variable
   let minusculas = name.slice(1)
-  console.log(mayuscula+minusculas)
+  //se retornan las dos variable concatenadas
   return mayuscula+minusculas
 }
 
 function invocarCallback(cb) {
   // Invoca al callback `cb`
   //Tu código:
+  return cb()
 }
 
 function operacionMatematica(n1, n2, cb) {
   //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
   //Devolver el callback pasándole como argumentos los números recibidos.
   //Tu código:
+  return cb(n1, n2)
 }
 
 function sumarArray(numeros, cb) {
@@ -27,12 +33,20 @@ function sumarArray(numeros, cb) {
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
   //Tu código:
+  let suma = null;
+  numeros.forEach((element)=>{
+    suma = suma + element
+  })
+  cb(suma)
 }
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
+  array.forEach(element=>{
+    cb(element)
+  })
 }
 
 function map(array, cb) {
@@ -40,12 +54,20 @@ function map(array, cb) {
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
+  const arreglo = array.map(element=>{
+    return cb(element)
+  })
+  return arreglo
 }
 
 function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
+  let filtrado = array.filter(element=>{
+    return element.charAt(0)=='a'
+  })
+  return filtrado
 }
 
 // No modificar nada debajo de esta línea
